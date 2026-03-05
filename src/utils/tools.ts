66bfbc5e599b79255ceb79321177785c5458e975
Yevin <yuwb0521@outlook.com>
2026-03-05 10:14:22 +0800

    chore:clear-project-for-full-refactor

--- src/utils/tools.ts
+++
@@ -1,6 +0,0 @@
-export const isMobile = (): boolean => {
-    return (
-        typeof window.orientation !== "undefined" ||
-        /IEMobile|iPhone|iPad|Android.*Mobile/i.test(navigator.userAgent)
-    );
-};
